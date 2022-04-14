da28a3d541b1743c4568b176977e55f2005a06e0
Kanton99 <avolkov99@gmail.com>
2022-04-14 14:25:23 +0200

    Revert "Tried automating cube building"
    This reverts commit e5c98bef83ac44b22f91c8a6708aa8e1ad0d5702.

---
+++ Homework1/Table.js
@@ -0,0 +1,46 @@
+//////////////////////
+// Table
+/////////////////////
+
+class Table extends Entity{
+
+    constructor(gl){
+        super(gl);
+        //this.gen_vertices();
+        this.verticies = [
+            //Top square
+            vec4(-0.5, 0.4,  0.5, 1.0),
+            vec4(-0.5,  0.5,  0.5, 1.0),
+            vec4(0.5,  0.5,  0.5, 1.0),
+            vec4(0.5, 0.4,  0.5, 1.0),//3
+
+            vec4(-0.5, 0.4, -0.5, 1.0),
+            vec4(-0.5,  0.5, -0.5, 1.0),
+            vec4(0.5,  0.5, -0.5, 1.0),
+            vec4(0.5, 0.4, -0.5, 1.0),//7
+            //front left leg
+            vec4(-0.5,-0.5,-0.5,1),
+            vec4(-0.4,-0.5,-0.5,1),
+            vec4(-0.4,0.4,-0.5,1),
+            vec4(-0.5,-0.5,-0.4,1),//11
+            vec4(-0.5,0.4,-0.4,1),
+            vec4(-0.4,0.4,-0.4,1),
+            vec4(-0.4,-0.5,-0.4,1)
+
+        ]
+        //main surface
+        this.make_quad(1, 0, 3, 2);
+        this.make_quad(2, 3, 7, 6);
+        this.make_quad(3, 0, 4, 7);
+        this.make_quad(6, 5, 1, 2);
+        this.make_quad(4, 5, 6, 7);
+        this.make_quad(5, 4, 0, 1);
+        //front left leg
+        this.make_quad(4,10,9,8);
+        this.make_quad(4,8,11,12);
+        this.make_quad(13,14,9,10);
+        this.make_quad(13,12,11,14);
+        this.make_quad(8,9,14,11);
+    }
+
+}
